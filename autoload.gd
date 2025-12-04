ec77fd0a4f31c607e7617175a2b53cc3ceaf76b9
melon <jermaise@gmail.com>
2025-12-03 21:23:51 -0700

    GAMBLING
    So I've added a function for 3stars, 4 stars and 5 stars that will return an item based on rng. For the 5 stars, I added a system where you can lose the 50/50 to DJUNGLESKOG which allows you to get guranteed Blahaj the next time

--- autoload.gd
+++ autoload.gd
@@ -4,7 +4,8 @@ extends Node
 var pity = 0
 var star5pity := 0
 var star4pity := 0
-
+var randomizes = 0
+var fiftes = false
 #func that aims to return a random number based on rolls
 func gamba(result):
 #pity is how many total pulls you've done, while roll is the number that determines your result
@@ -13,14 +14,14 @@ func gamba(result):
 	var roll = randi_range(1,70)
 	var divisor = roll % 10
 	if roll == 66:
-		result = str('5star')
+		result = star5()
 		print('g')
 	elif star4pity == 10:
-		result = str('4star')
+		result = star4()
 		star4pity = 0
 		print('p')
 	elif divisor == 0:
-		result = str('4star')
+		result = star4()
 		pity += 1
 		print('p')
 	elif pity >= 55:
@@ -28,17 +29,51 @@ func gamba(result):
 		var star5pity = randi_range(luck,15)
 		if star5pity == 15:
 			pity = 0
-			result = str('5star')
+			result = star5()
 			print('g')
 		else:
 			pity += 1
-			result = str('3star')
+			result = star3()
 			print('b')
 	else:	
 		print('b')
-		result = str('3star')
-	print(pity)
+		result = star3()
+	print(result)
 	return result
+	
+#randomize the actual value of 3stars - 5stars
+
+func star3():
+	randomizes = randi_range(1,4)
+	if randomizes == 1:
+		return str('VINTERFINT (Bottle)')
+	elif randomizes == 2:
+		return str('DRÖNJÖNS')
+	elif randomizes == 3:
+		return str('VINTERFINT (Candle)')
+	elif randomizes == 4:
+		return str('SMYCKA')
+func star4():
+	randomizes = randi_range(1,3)
+	if randomizes == 1:
+		return str('UPPSTA')
+	elif randomizes == 2:
+		return str('BILLY')
+	elif randomizes == 3:
+		return str('VAPPEBY')
+		
+func star5():
+	if fiftes == true:
+		fiftes = false
+		return str('Blahaj')
+	elif fiftes == false:
+		randomizes = randi_range(1,2)
+		if randomizes == 1:
+			return str('Blahaj')
+		elif randomizes == 2:
+			fiftes = true
+			return str('DJUNGLESKOG (Bear)')
+			
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
