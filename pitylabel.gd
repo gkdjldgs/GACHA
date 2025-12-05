2206f352c13b5c591de08035ae590e130e287ee1
melon <jermaise@gmail.com>
2025-12-04 18:10:32 -0700

    FONTS
    I added fonts to the game in preparation  to add actual text. The font is Sacrifice by Heinzel Studio

--- pitylabel.gd
+++ pitylabel.gd
@@ -1,6 +1,6 @@
-extends Button
+extends RichTextLabel
 
-
+var texts = str('PITY')
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,9 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_pressed() -> void:
-	var test = ''
-	var text = autoload.gamba(test)
-	print(text)
